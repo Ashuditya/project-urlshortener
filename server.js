ce8fb86c0a3e3ffba5f565029986b0447e487ec1
Ashuditya <ashuditya@hotmail.com>
2021-11-25 21:51:49 +0530

    added api call for /api/shorturl

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
 const express = require('express');
+var bodyParser = require("body-parser");
 const cors = require('cors');
 const app = express();
 
+var num = null;
+var add = null;
+
 // Basic Configuration
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
 
 app.use(cors());
@@ -14,6 +20,23 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/shorturl", (req,res) =>{
+  console.log(req.body);
+  var s = '';
+  if (req.body.url.startsWith("http://"))  
+    res.json({"url": req.body.url, "short_url": Math.floor(Math.random() * 10000)});
+  else
+    res.json({"error": "invalid url"});
+});
+app.get("/api/shorturl/:surl", (req,res) => {
+  var {surl} = req.params;
+  if (surl===num){
+    
+  }else{
+
+  }
+  
+});
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
